8696efdf658afa86548b752f8483f647e3c178a8
Aayush Agrawal <aayushagrawal135@gmail.com>
2022-09-13 12:11:59 -0400

    Add clicks on state

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
     $(".map").maphilight();
+
+    $(".area").click(function () {
+      var data = $(this).data("maphilight") || {};
+      data.alwaysOn = true;
+      $(this).data("maphilight", data).trigger("alwaysOn.maphilight");
+    });
 });
